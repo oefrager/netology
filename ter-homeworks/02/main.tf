fbc7901901aa75c31d9741530a6e4dcf85675d8a
Oleg <83029093+oefrager@users.noreply.github.com>
2025-01-31 10:23:26 +0400

    Create main.tf

---
+++ ter-homeworks/02/main.tf
@@ -0,0 +1,43 @@
+resource "yandex_vpc_network" "develop" {
+  name      = var.vpc_name
+}
+resource "yandex_vpc_subnet" "develop" {
+  name           = var.vpc_name
+  zone           = var.default_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr
+}
+
+#--------------
+data "yandex_compute_image" "ubuntu" {
+  family = var.vm_web_image
+}
+
+resource "yandex_compute_instance" "platform" {
+  name            = var.vm_web_instance["name"]
+  platform_id     = var.vm_web_instance["platform_id"]
+  resources {
+    cores         = var.vm_web_instance["cores"]
+    memory        = var.vm_web_instance["memory"]
+    core_fraction = var.vm_web_instance["core_fraction"]
+  }
+#--------------
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "${file(var.vms_ssh_root_key)}"
+  }
+}
