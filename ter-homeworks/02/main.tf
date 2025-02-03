c755da31ce5fa4e33d15d811dfbc4f0b3be36f5c
Oleg <83029093+oefrager@users.noreply.github.com>
2025-02-03 15:57:36 +0400

    Update main.tf

--- ter-homeworks/02/main.tf
+++ ter-homeworks/02/main.tf
@@ -1,6 +1,8 @@
 resource "yandex_vpc_network" "develop" {
   name      = var.vpc_name
 }
+
+# Web VM----------------------------------------
 resource "yandex_vpc_subnet" "develop" {
   name           = var.vpc_name
   zone           = var.default_zone
@@ -8,20 +10,19 @@ resource "yandex_vpc_subnet" "develop" {
   v4_cidr_blocks = var.default_cidr
 }
 
-#--------------
 data "yandex_compute_image" "ubuntu" {
   family = var.vm_web_image
 }
 
 resource "yandex_compute_instance" "platform" {
-  name            = var.vm_web_instance["name"]
-  platform_id     = var.vm_web_instance["platform_id"]
+  #name            = var.vm_web_instance["name"]
+  name            = local.vm_web
+  platform_id     = var.vms_resources.web.platform_id
   resources {
-    cores         = var.vm_web_instance["cores"]
-    memory        = var.vm_web_instance["memory"]
-    core_fraction = var.vm_web_instance["core_fraction"]
+    cores         = var.vms_resources.web.cores
+    memory        = var.vms_resources.web.memory
+    core_fraction = var.vms_resources.web.core_fraction
   }
-#--------------
 
   boot_disk {
     initialize_params {
@@ -35,9 +36,39 @@ resource "yandex_compute_instance" "platform" {
     subnet_id = yandex_vpc_subnet.develop.id
     nat       = true
   }
+  metadata = var.metadata
+}
 
-  metadata = {
-    serial-port-enable = 1
-    ssh-keys           = "${file(var.vms_ssh_root_key)}"
+# db VM----------------------------------------------
+resource "yandex_vpc_subnet" "db" {
+  name           = var.vpc_name_db
+  zone           = var.default_zone_db
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr_db
+}
+
+resource "yandex_compute_instance" "db" {
+#  name            = var.vm_db_instance["name"]
+  name            = local.vm_db
+  platform_id     = var.vms_resources.db.platform_id
+  zone            = var.default_zone_db
+  resources {
+    cores         = var.vms_resources.db.cores
+    memory        = var.vms_resources.db.memory
+    core_fraction = var.vms_resources.db.core_fraction
   }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.db.id
+    nat       = true
+  }
+  metadata = var.metadata
 }
